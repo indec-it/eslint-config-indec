052c3213835e5a880c94c49db27f5938cfdacdc2
Damián Berruezo <damianberruezo@gmail.com>
2018-10-05 19:22:58 -0300

    fix(all): remove objects and improve some rules

--- browser.js
+++ browser.js
@@ -1,19 +1,3 @@
-const rules = {
-    lodash: {
-        'lodash/prop-shorthand': ['error', 'never'],
-        'lodash/matches-prop-shorthand': ['error', 'never'],
-        'lodash/prefer-lodash-method': ['error', {
-            ignoreMethods: ['assign', 'find', 'map']
-        }]
-    },
-    react: {
-        'react/descructuring-assignment': 'off',
-        'react/jsx-indent': ['error', 4],
-        'react/jsx-indent-props': ['error', 4],
-        'react/jsx-tag-spacing': ['error', {beforeSelfClosing: 'never'}],
-    }
-};
-
 module.exports = {
     parser: 'babel-eslint',
     plugins: [
@@ -23,18 +7,31 @@ module.exports = {
     ],
     extends: 'airbnb',
     rules: {
-        'arrow-parens': ['error', 'as-needed'],
-        'comma-dangle': ['error', 'never'],
-        'function-paren-newline': 'off',
-        'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
-        'lines-between-class-members': ['error', 'always', {exceptAfterSingleLine: true}],
-        'max-len': ['error', 120],
-        'no-underscore-dangle': ['error', {allow: ['_id']}],
+        'lodash/prop-shorthand': ['error', 'never'],
+        'lodash/matches-prop-shorthand': ['error', 'never'],
+        'lodash/prefer-lodash-method': ['error', {
+            ignoreMethods: ['assign', 'find', 'map']
+        }],
+
         'jsx-a11y/anchor-is-valid': ['error', {
             specialLink: [],
             aspects: ['invalidHref', 'noHref', 'preferButton']
         }],
-        ...rules.lodash,
-        ...rules.react
+
+        'react/descructuring-assignment': 'off',
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-tag-spacing': ['error', {beforeSelfClosing: 'never'}],
+
+        // Override Airbnb rules
+        'arrow-parens': ['error', 'as-needed'],
+        'comma-dangle': ['error', 'never'],
+        'function-paren-newline': 'off',
+        'max-len': ['error', {code: 120, ignoreStrings: true, ignoreTemplateLiterals: true}],
+        'max-statements-per-line': 'error',
+        'lines-between-class-members': ['error', 'always', {exceptAfterSingleLine: true}],
+        'no-underscore-dangle': ['error', {allow: ['_id']}],
+        'object-curly-spacing': ['error', 'never'],
+        'import/no-extraneous-dependencies': ['error', {devDependencies: true}]
     }
 };
--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const newRules = {
-    common: {
-        'arrow-parens': ['error', 'as-needed'],
-        'comma-dangle': ['error', 'never'],
-        'function-paren-newline': 'off',
-        'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
-        'max-len': ['error', 120],
-        'max-statements-per-line': 'error',
-        'no-param-reassign': 'off',
-        'no-return-assign': 'off',
-        'no-underscore-dangle': [
-            'error', {
-                allow: ['_id']
-            }
-        ],
-        'object-curly-spacing': ['error', 'never']
-    },
-    lodash: {
-        'lodash/import-scope': ['error', 'member'],
-        'lodash/prop-shorthand': ['error', 'never'],
-        'lodash/matches-prop-shorthand': ['error', 'never'],
-        'lodash/prefer-lodash-method': ['error', {
-            ignoreMethods: ['assign', 'find']
-        }]
-    }
-};
-
 module.exports = {
     plugins: [
         'lodash'
@@ -45,7 +18,23 @@ module.exports = {
         'prefer-const': ['error'],
         quotes: ['error', 'single'],
         semi: ['error', 'always'],
-        ...newRules.common,
-        ...newRules.lodash
+
+        'lodash/import-scope': ['error', 'member'],
+        'lodash/prop-shorthand': ['error', 'never'],
+        'lodash/matches-prop-shorthand': ['error', 'never'],
+        'lodash/prefer-lodash-method': ['error', {
+            ignoreMethods: ['assign', 'find']
+        }],
+
+        // Override Airbnb rules
+        'arrow-parens': ['error', 'as-needed'],
+        'comma-dangle': ['error', 'never'],
+        'function-paren-newline': 'off',
+        'max-len': ['error', {code: 120, ignoreStrings: true, ignoreTemplateLiterals: true}],
+        'max-statements-per-line': 'error',
+        'lines-between-class-members': ['error', 'always', {exceptAfterSingleLine: true}],
+        'no-underscore-dangle': ['error', {allow: ['_id']}],
+        'object-curly-spacing': ['error', 'never'],
+        'import/no-extraneous-dependencies': ['error', {devDependencies: true}]
     }
 };
